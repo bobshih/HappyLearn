61c95b787335f5f2247ebf4ca206b181d5c34d86
kevin <kevin15121@gmail.com>
2016-04-18 19:05:14 +0800

    gridsystem

---
+++ HappyLearnFrontPage/WebContent/js/menuSlidebar.js
@@ -0,0 +1,10 @@
+var main = function() {
+  /* Push the body and the nav over by 285px over */
+  $('.navMenuIconBg').click(function() {
+$('.navMenuSlidebar').toggle();
+
+  });
+};
+
+
+$(document).ready(main);
