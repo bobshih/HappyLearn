3a99241fedd46ef2e0439aa908662e128cc952dc
kevin <kevin15121@gmail.com>
2016-04-20 06:38:23 +0800

    course pop out window

---
+++ HappyLearnFrontPage/WebContent/js/coursePop.js
@@ -0,0 +1,15 @@
+var main = function() {
+  /* Push the body and the nav over by 285px over */
+  $('.courseZoneTitle').click(function() {
+    $('.coursePop').toggle();
+
+  });
+
+  $('.coursePopClose').click(function() {
+    $('.coursePop').toggle();
+
+  });
+};
+
+
+$(document).ready(main);
