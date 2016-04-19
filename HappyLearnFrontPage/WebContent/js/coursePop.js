fe522ecce0c0eecb362e0155755199392d3e4882
kevin <kevin15121@gmail.com>
2016-04-20 06:46:59 +0800

    course pop out window: courseFrame onclick event

--- HappyLearnFrontPage/WebContent/js/coursePop.js
+++ HappyLearnFrontPage/WebContent/js/coursePop.js
@@ -1,9 +1,5 @@
 var main = function() {
   /* Push the body and the nav over by 285px over */
-  $('.courseZoneTitle').click(function() {
-    $('.coursePop').toggle();
-
-  });
 
   $('.coursePopClose').click(function() {
     $('.coursePop').toggle();
@@ -11,5 +7,8 @@ var main = function() {
   });
 };
 
+function coursePopToggle(){
+  $('.coursePop').toggle();
+}
 
 $(document).ready(main);
